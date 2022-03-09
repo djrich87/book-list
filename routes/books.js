0d6a62c21c5e4abd3e9575c564e0192401215923
djrich87 <djrich87@gmail.com>
2022-03-09 09:37:56 -0700

    add book list

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,9 @@ import * as booksCtrl from '../controllers/books.js'
 
 const router = Router()
 
+router.get('/', booksCtrl.index)
 router.get('/new', booksCtrl.new)
+router.post('/', booksCtrl.create)
 
 
 
